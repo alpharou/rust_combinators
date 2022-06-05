3671230005562a9c466e2f1043240b580a8a610c
alpharou <alpharou.dev@gmail.com>
2022-06-05 15:45:55 +0200

    Initial state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct Student {
+	name: String,
+	gpa: f32,
 }
+fn main() {
+
+	let students = vec![
+		"Bogdan 3.1",
+		"Wallace 2.3",
+		"Lidiya 3.5",
+		"Kyle 3.9",
+		"Anatoliy 4.0",
+	];
+
+}
+//Create vector of Student objects.
+//Only include students with a GPA 3.5 or higher
